1ee9d1afd497649d94d0d2bfdf5eb2de4ccc30a7
jordanwheeler7 <122232837+jordanwheeler7@users.noreply.github.com>
2023-02-13 20:32:07 -0500

    Project Commits

---
+++ dividebyzero_output.py
@@ -0,0 +1,16 @@
+"""
+    PS C:\Users\jorda\OneDrive\Desktop\Northwest Homework\DATA ANALYTICS FUNDAMENTALS\New folder\datafun-05-data-at-rest> & C:/Users/jorda/AppData/Local/Programs/Python/Python311/python.exe "c:/Users/jorda/OneDrive/Desktop/Northwest Homework/DATA ANALYTICS FUNDAMENTALS/New folder/datafun-05-data-at-rest/dividebyzero.py"
+Enter numerator: 100  
+Enter denominator: 0
+Attempted to divide by zero
+
+Enter numerator: 100
+Enter denominator: hello
+You must enter two integers
+
+Enter numerator: 100
+Enter denominator: 7
+ 100.000 / 7.000 =  14.286
+PS C:\Users\jorda\OneDrive\Desktop\Northwest Homework\DATA ANALYTICS FUNDAMENTALS\New folder\datafun-05-data-at-rest> 
+       
+    """
